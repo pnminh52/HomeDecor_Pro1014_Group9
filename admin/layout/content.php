e2c2328ce995b94b8a65751d615b077840513084
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-18 07:49:45 +0700

    admin layout

--- admin/layout/content.php
+++ admin/layout/content.php
@@ -1,5 +1,20 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="<?=$ADMIN_URL?>" class="app-brand-link"></a>
+        <a href="<?=$ADMIN_URL?>" class="app-brand-link">
+        <span class="app-brand-logo demo"></span>
+        <span class="app-brand-text demo menu-text fw-bolder ms-2">HomeDecor</span>
+        </a>
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+              <i class="bx bx-chevron-left bx-sm align-middle"></i>
+            </a>
     </div>
+    <div class="menu-inner-shadow"></div>
+    <ul class="menu-inner py-1">
+        <li class="menu-item">
+        <a href="<?=$ADMIN_URL?>/view/overview" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div data-i18n="Analytics">Tổng quan</div>
+              </a>
+        </li>
+    </ul>
 </aside>
