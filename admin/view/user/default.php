f90c87d300431bf1c505c7478b4ac33b7fd4dc21
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2024-11-18 08:59:09 +0700

    userCRUD

--- admin/view/user/default.php
+++ admin/view/user/default.php
@@ -1,1 +1,18 @@
 <div >day la trang default user</div>
+<div > danh sach khach hang </div>
+<form action="" method="post">
+    <a href="">Them khach hang</a>
+    
+<table>
+    <tr>
+        <th>chon</th>
+        <th>id</th>
+        <th>name</th>
+        <th>email</th>
+        <th>sdt</th>
+        <th>dia chi</th>
+        <th>quen han</th>
+        <th>action</th>
+    </tr>
+</table>
+</form>
